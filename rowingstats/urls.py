c1fc6757cae30b21c987c1256fb067769bb0986c
Charles Barry <charlesawake@gmail.com>
2018-02-20 23:37:42 +0000

    tweaks to blog

--- rowingstats/urls.py
+++ rowingstats/urls.py
@@ -19,6 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^articles/', include('blog.urls')),
-	url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 	url(r'^', include('rowing.urls')),
 ]
