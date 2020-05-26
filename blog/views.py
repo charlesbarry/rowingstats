8643b2b725496df14d098d34f20d2afcb9c29191
charlesbarry <charlesawake@gmail.com>
2020-05-26 02:39:03 +0100

    updates to blog template and switch to markdown

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 
 from django.views.generic import ListView, DetailView, UpdateView, TemplateView
 from .models import Article
+import markdown
 
 # index view: list of all articles
 class ArticleList(ListView):
-	model = Article
-	paginate_by = 5
-	ordering = ['-published']
-	queryset = Article.objects.filter(public=1)
-	
-class ArticleDetail(DetailView):
-	model = Article
+    model = Article
+    paginate_by = 5
+    ordering = ['-published']
+    queryset = Article.objects.filter(public=1)
+    
+class ArticleDetail2(DetailView):
+    model = Article
+    
+def ArticleDetail(request, pk):
+    article = Article.objects.get(pk=pk)
+    md = markdown.markdown(article.content)
+
+    return render(request, 'blog/article_detail.html', {'object': article, 'content': md})
--- rowingstats/urls.py
+++ rowingstats/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^blog/', include('blog.urls')),
 	url(r'^articles/', include('blog.urls')),
 	url(r'^hrr/', include('hrr.urls')),
 	url(r'^', include('rowing.urls')),
